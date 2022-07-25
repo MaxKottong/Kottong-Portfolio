9081700aef09ff74cd1ef0270770c2bc4d276089
Max Kottong <max.kottong@gmail.com>
2022-07-25 14:30:27 -0400

    css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,3 @@
-$("[data-trigger]").on("click", function () {
-    var trigger_id = $(this).attr('data-trigger');
-    $(trigger_id).toggleClass("show");
-    $('body').toggleClass("offcanvas-active");
-})
-
-$(".btn-close").click(function (e) {
-    $(".navbar-collapse").removeClass("show");
-    $("body").removeClass("offcanvas-active");
-}); 
-
-$('.navbar-nav>li>a').on('click', function () {
-    $('.navbar-collapse').collapse('hide');
-});
-
 $(window).on('resize', function () {
     if ($(window).width() < 576) {
         $("#contact-list").addClass("d-block");
