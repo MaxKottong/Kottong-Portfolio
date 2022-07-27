ca0cc3924a8e8d254dad106496c88cc3a8b3cb65
Max Kottong <max.kottong@gmail.com>
2022-07-27 19:02:32 -0400

    About me update

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-$(window).on('resize', function () {
-    if ($(window).width() < 576) {
-        $("#contact-list").addClass("d-block");
-        $("#contact-list").removeClass("d-flex");
-    } else {
-        $("#contact-list").addClass("d-flex");
-        $("#contact-list").removeClass("d-block");
-    }
-})
